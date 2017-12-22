ad5940a859284bd899364d9f4932a7d349c37473
unknown <jiangshui.lin@fubon.com.cn>
2017-12-22 15:09:42 +0800

    first up

---
+++ src/main/java/com/linebook/MenageMasterApplication.java
@@ -0,0 +1,12 @@
+package com.linebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenageMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenageMasterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/linebook/menagemaster/MenageMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.linebook.menagemaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MenageMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
